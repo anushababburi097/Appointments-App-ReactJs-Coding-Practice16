28400dbfa82e4a8be7752af0b25d9360c52f4bfe
anushababburi097 <128217277+anushababburi097@users.noreply.github.com>
2023-06-02 14:38:10 +0530

    Update index.js

--- src/components/AppointmentItem/index.js
+++ src/components/AppointmentItem/index.js
@@ -1,28 +1,33 @@
 // Write your code here
 import './index.css'
 
-const AppointmentItem = props => {
-  const {appointmentDetails, toggleIsFavorite} = props
-  const {name, date, isFilterActive, id} = appointmentDetails
-
-  const starImgUrl = isFilterActive
+const AppointmentIem = props => {
+  const {appointmentDetails, toggleIsStarred} = props
+  const {id, title, date, isStarred} = appointmentDetails
+  const starImgUrl = isStarred
     ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
     : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
 
-  const onClickFavoriteIcon = () => {
-    toggleIsFavorite(id)
+  const onClickStar = () => {
+    toggleIsStarred(id)
   }
 
   return (
     <li className="appointment-item">
-      <div>
-        <h1>{name}</h1>
-        <button type="button" onClick={onClickFavoriteIcon}>
-          <img src={starImgUrl} alt="star" className="favorite-icon" />
+      <div className="header-container">
+        <p className="title">{title}</p>
+        <button
+          type="button"
+          data-testid="star"
+          className="star-button"
+          onClick={onClickStar}
+        >
+          <img src={starImgUrl} className="star" alt="star" />
         </button>
       </div>
-      <p>Date {date}</p>
+      <p className="date">Date: {date}</p>
     </li>
   )
 }
-export default AppointmentItem
+
+export default AppointmentIem
